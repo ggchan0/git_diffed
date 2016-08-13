82bfb8b1f1f558c337601a59a3c1e33423a1d9f1
Garrett Chan <ggchan@calpoly.edu>
2016-08-13 16:00:48 -0700

    first commit for node stuff

---
+++ server.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+var main = require('./routes/main');
+
+app.use('/', main);
+
+app.listen(3000);
+console.log("Magic happening on port 3000");
