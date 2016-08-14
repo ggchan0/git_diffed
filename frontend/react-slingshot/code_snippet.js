d7010d6283e2217c9767b6011d94066faeaf8698
kavithadee <kdhanukodi@ucdavis.edu>
2016-08-14 09:40:55 -0700

    Add files via upload
    Code snippet in HTML, CSS, and (tentative) JSX

---
+++ frontend/react-slingshot/code_snippet.js
@@ -0,0 +1,44 @@
+var NewComponent = React.createClass({
+    render: function () {
+        return ( < div >
+            < div className = "outer" >
+            < div className = "contentleft" >
+            < img src = "github%20anon.png" / >
+            < p > Pauline Smith < /p> < p id = "username" > @pauline < /p> < p id = "para" >
+            import Foundation
+            import CoreGraphics public class ChartBaseDataSet: NSObject, IChartDataSet {
+                '{'
+            }
+            public required override init() {
+                '{'
+            }
+            super.init() // default color colors.append(NSUIColor(red: 140.0/255.0, green: 234.0/255.0, blue: 255.0/255.0, alpha: 1.0)) valueColors.append(NSUIColor.blackColor()) {'}'} public init(label: String?) {'{'} super.init() // default color colors.append(NSUIColor(red: 140.0/255.0, green: 234.0/255.0, blue: 255.0/255.0, alpha: 1.0))
+            < /p> < p id = "comment" > No Comments < /p> < div className = "tableleft" >
+            < p >
+            < strong > Project < /strong> < /p> < p >
+            < strong > Language < /strong> < /p> < p >
+            < strong > IDE < /strong> < /p> < p >
+            < strong > Date Created < /strong> < /p> < p >
+            < strong > Date Modified < /strong> < /p> < /div> < div className = "tableright" >
+            < p > IOS Application < /p> < p > Swift < /p> < p > Xcode < /p> < p > 8 / 2 / 16 < /p> < p > 8 / 16 / 16 < /p> < /div> < /div> < div className = "contentright" >
+            < img src = "github%20anon.png" / >
+            < p > Joe Baker < /p> < p id = "username" > @joeb < /p> < p id = "para" >
+            import Foundation
+            import CoreGraphics public class ChartBaseDataSet: NSObject, IChartDataSet {
+                '{'
+            }
+            public required override init() {
+                '{'
+            }
+            super.init() // default color colors.append(NSUIColor(red: 140.0/255.0, green: 234.0/255.0, blue: 255.0/255.0, alpha: 1.0)) valueColors.append(NSUIColor.blackColor()) {'}'} public init(label: String?) {'{'} super.init() // default color colors.append(NSUIColor(red: 140.0/255.0, green: 234.0/255.0, blue: 255.0/255.0, alpha: 1.0))
+            < /p> < p id = "comment" > No Comments < /p> < div className = "tableleft" >
+            < p >
+            < strong > Project < /strong> < /p> < p >
+            < strong > Language < /strong> < /p> < p >
+            < strong > IDE < /strong> < /p> < p >
+            < strong > Date Created < /strong> < /p> < p >
+            < strong > Date Modified < /strong> < /p> < /div> < div className = "tableright" >
+            < p > IOS Application < /p> < p > Swift < /p> < p > Eclipse < /p> < p > 3 / 7 / 14 < /p> < p > 3 / 12 / 14 < /p> < /div> < /div> < /div> < /div>
+        );
+    }
+});
