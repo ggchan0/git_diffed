8791286ea028664b0637eeb43632f78e94435df6
Leland Lee <lelandlee4@gmail.com>
2016-08-14 20:34:53 -0700

    some small ui cange WIP

--- frontend/react-slingshot/src/components/CompareForm.js
+++ frontend/react-slingshot/src/components/CompareForm.js
@@ -1,5 +1,5 @@
 import React, {PropTypes} from 'react';
-// import * as d3 from 'd3';
+import * as d3 from 'd3';
 
 class CompareForm extends React.Component {
   constructor(props, context) {
@@ -32,24 +32,25 @@ class CompareForm extends React.Component {
     const { meta1, meta2 } = this.props;
 
     // Would be fun to have an arc around the avator to show what they are writing in etc.
-    // const r = 125;
-    // const arc = d3.arc()
-    //   .innerRadius(r)
-    //   .outerRadius(r + 5);
+    const r = 125;
+    const border = 5;
+    const arc = d3.arc()
+      .innerRadius(r)
+      .outerRadius(r + border);
+    const pie = d3.pie()
+      .padAngle(.02);
+    const color = d3.scaleOrdinal(d3.schemeCategory10);
 
-    // const pie = d3.pie()
-    //   .padAngle(.02);
-
-    // const data = [12, 32, 15, 21];
+    const data = [12, 32, 15, 21];
 
 
     return (
       <div style={{display: "inline-block", width: "100%", margin: "50px 0"}}>
         <div className="person-left" style={{float:"left", width: "49.75%"}}>
           <div style={{textAlign: "center"}}>
-            {/*<svg style={{width: 2*(r+5), height: 2*(r+5)}}>
-              {pie(data).map((slice, i) => <path d={arc(slice)} transform={`translate(${r+5},${r+5})`} key={i} fill={"maroon"} />)}
-            </svg>*/}
+            {<svg style={{width: 2*(r+border), height: 2*(r+border)}}>
+              {pie(data).map((slice, i) => <path d={arc(slice)} transform={`translate(${r+border},${r+border})`} key={i} fill={color(i)} />)}
+            </svg>}
             <img src={meta1.avatar_url} style={{borderRadius:"50%", width: "250px", border: "5px solid #CCCCCC"}} />
             <h1>{meta1.name}</h1>
             <h2 style={{color: "#666"}}>{meta1.login}</h2>
